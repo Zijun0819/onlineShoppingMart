bbcd4b84074f04dfb5a2345ee2bef7591bcc11b9
Zijun0819 <1024994903@qq.com>
2025-01-22 20:15:18 +0800

    linux-deployment

---
+++ start_django.sh
@@ -0,0 +1,5 @@
+# /home/zijun/work/onlineShoppingMart/start_django.sh
+#!/bin/bash
+source /home/zijun/work/onlineShoppingMart/venv/bin/activate
+/home/zijun/work/onlineShoppingMart/venv/bin/python /home/zijun/work/onlineShoppingMart/manage.py runserver 0.0.0.0:3002
+
